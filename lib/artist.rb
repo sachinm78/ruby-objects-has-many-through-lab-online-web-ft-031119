68e6246449a776c5306a6d931ad6a01ddeccf0db
Sachin Mahajan <sachinm78@gmail.com>
2019-03-26 18:43:51 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,4 +1,18 @@
 class Atrist 
+  attr_accessor :name, :song, :genre
   
+  @@all = []
+  
+  def intialize(name)
+    @name = name
+    @@all << name
+  end
+  
+  
+  
+  
+  def self.all 
+    @@all
+  end
   
 end
