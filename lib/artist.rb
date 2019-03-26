18f3ba8eed2be9de1d0a188574d0afc271f6579f
Sachin Mahajan <sachinm78@gmail.com>
2019-03-26 18:53:17 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,20 +1,23 @@
 class Artist 
-  attr_accessor :name
-  
-  @@all = []
-  
-  def initialize(name)
-    @name = name
-    @@all << name
-  end
-  
-  def new_song(name, genre)
-    
-  end
-  
-  
-  def self.all 
-    @@all
-  end
-  
+	attr_accessor :name, :songs
+	
+	def initialize(name)
+		@name = name
+		@songs = []
+	end
+	
+	def add_song(song)
+		@songs << song
+		song.artist = self
+	end
+	
+	def songs #used as self.songs in method below
+		@songs
+	end
+	
+	def genres #returns an array of genre objects
+		self.songs.collect do |song|
+			song.genre 
+		end
+	end
 end
